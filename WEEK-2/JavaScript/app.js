86a32ceb09f52d62734a5c0ad4fc012aa291366a
arafat-yasin-2413 <129652012+arafat-yasin-2413@users.noreply.github.com>
2023-12-31 23:11:52 +0600

    first commit

---
+++ WEEK-2/JavaScript/app.js
@@ -0,0 +1,56 @@
+// console.log("Hello World")
+// var number = "This is string";
+// // console.log(number)
+
+// var lastname="Uddin"
+// console.log(lastname)
+
+// console.log(typeof(lastname))
+
+
+// 5-5
+
+// var weather ="rainy"; 
+
+// if (weather=="hot") {
+//     console.log("Ami Baire Jabo");
+// }
+
+// else if(weather=="rainy") {
+//     console.log("Chata niye baire jabo")
+// }
+
+// else {
+//     console.log("Baire Jabo na");
+// }
+
+
+// console.log(weather=="cold");
+
+
+
+
+
+// 5-6 Objects
+
+
+// 5-7 Array
+
+// var friends=["Moshiur" ,"Sajedul", 'Sakib']
+// console.log(friends[0]);
+// console.log(friends.length);
+
+
+
+// 5-8
+// for (var i = 1; i<10; i++) {
+//     console.log(i);
+    
+// }
+
+var arr=[10,15,20,25,30];
+for (var i = 0; i<arr.length; i++){
+    if (arr[i] % 2 == 0){
+        console.log(arr[i]);
+    }
+}
